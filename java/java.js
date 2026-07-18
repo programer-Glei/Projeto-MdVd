d85f22f4e993e24fb9ccd998a77af9e27131324f
Gleibson dos Santos Pereira <gleibson.santos05@gmail.com>
2026-07-18 13:13:01 -0300

    proximo commit

--- java/java.js
+++ java/java.js
@@ -7,10 +7,15 @@ let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
 let thumbnailItemDom = thumbnailBorderDom.querySelectorAll('.item');
 let timeDom = document.querySelector('.carousel .time');
 
+thumbnailBorderDom.appendChild(thumbnailItemDom[0]);
+
 nexDom.onclick = function(){
     showSlider('next');
     console.log("gdgsdgsçgljgçlfgj");
 }
+
+let timeRunning = 2000;
+let runTimeout;
 
 function showSlider(type){
     let SliderItemDom = SliderDom.querySelectorAll('.carousel .list .item');
@@ -20,5 +25,15 @@ function showSlider(type){
         SliderDom.appendChild(SliderItemDom[0]);
         thumbnailBorderDom.appendChild(thumbnailItemDom[0]);
         carouselDom.classList.add('next');
+    }else{
+        SliderDom.prepend(SliderItemDom[SliderItemDom - 1]);
+        thumbnailBorderDom.prepend(thumbnailItemDom.length - 1);
+        carouselDom.classList.add('prev');
     }
+
+    clearTimeout(runTimeout);
+    runTimeout = setTimeout(() =>{
+        carouselDom.classList.remove('next');
+        carouselDom.classList.remove('prev');
+    }, timeRunning);
 }
