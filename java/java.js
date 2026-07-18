a19a73240cf423819303f9cd631fa677a28176b2
Gleibson dos Santos Pereira <gleibson.santos05@gmail.com>
2026-07-18 12:12:36 -0300

    proximo commit

--- java/java.js
+++ java/java.js
@@ -5,12 +5,20 @@ let carouselDom = document.querySelector('.carousel');
 let SliderDom = carouselDom.querySelector('.carousel .list');
 let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
 let thumbnailItemDom = thumbnailBorderDom.querySelectorAll('.item');
+let timeDom = document.querySelector('.carousel .time');
 
 nexDom.onclick = function(){
     showSlider('next');
+    console.log("gdgsdgsçgljgçlfgj");
 }
 
 function showSlider(type){
     let SliderItemDom = SliderDom.querySelectorAll('.carousel .list .item');
     let thumbnailItemDom = document.querySelectorAll('.carousel .thumbnail .item');
+
+    if(type == 'next'){
+        SliderDom.appendChild(SliderItemDom[0]);
+        thumbnailBorderDom.appendChild(thumbnailItemDom[0]);
+        carouselDom.classList.add('next');
+    }
 }
